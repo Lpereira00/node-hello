95f2c6c9477d023368d7e2fb306cf684d7e83fc6
lcerdas <luis.pereira0015189@gmail.com>
2022-02-02 12:45:27 -0600

    Adding rds connection and change buildspec.yml

--- index.js
+++ index.js
@@ -10,27 +10,3 @@ const server = http.createServer((req, res) => {
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`);
 });
-
-var express = require('express');
-var router = express.Router();
-var mysql = require('mysql');
-var data;
-var my_database = mysql.createConnection({
-  host: '$YOUR_DATABASE ENDPOINT',
-  port : '3306',
-  user : 'admin',
-  password: 'admin123',
-  database: 'node-test'
-  });
-my_database.connect(function(err){
-  if (err) throw err;
-  console.log("connected");
-  my_database.query("SELECT * from $YOUR_TABLE", function(err,   result, fields){
-    if (err) throw err;
-    data = result;
-  });
-});
-router.get('/', function(req, res, next){
-res.send(JSON.stringify(data));
-});
-module.exports = router;
