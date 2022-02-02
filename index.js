9f286c7c03e4ae8e8e21a0239d830ca9b69772fb
lcerdas <luis.pereira0015189@gmail.com>
2022-02-02 16:12:19 -0600

    changing things

--- index.js
+++ index.js
@@ -1,27 +1,17 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello Node! My Test Pipeline\n'
-  res.end(msg);
-});
-
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
-});
-
-var express = require('express');
-var router = express.Router();
-var mysql = require('mysql');
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql');
 var data;
+var json;
 
 var my_database = mysql.createConnection({
   host: 'database-1.ceryvyudjtn7.us-east-1.rds.amazonaws.com',
-port : '3306',
-user : 'admin',
-password: 'admin123',
-database: 'test_node'
+  port : '3306',
+  user : 'admin',
+  password: 'admin123',
+  database: 'test_node'
 });
 
 my_database.connect(function(err){
@@ -32,7 +22,19 @@ my_database.connect(function(err){
     data = result;
   });
 });
-router.get('/', function(req, res, next){
-res.send(JSON.stringify(data));
+
+// router.get('/', function(req, res, next){
+//   json = res.send(JSON.stringify(data));
+// });
+// module.exports = router;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  const msg = 'Hello Node! My Test Pipeline\n'
+  res.end("<p>"+msg+"</p>"+"<p>"+JSON.stringify(data)+"</p>");
 });
-module.exports = router;
+
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/`);
+});
+
