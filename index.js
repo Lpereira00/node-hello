d951269e9f8cd323a0b0cf1d1c470fb6541360e5
lcerdas <luis.pereira0015189@gmail.com>
2022-02-02 13:08:42 -0600

    Adding rds connection and change buildspec.yml

--- index.js
+++ index.js
@@ -10,3 +10,22 @@ const server = http.createServer((req, res) => {
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`);
 });
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : "database-1.ceryvyudjtn7.us-east-1.rds.amazonaws.com",
+  user     : "admin",
+  password : "admin123",
+  port     : "3306"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
+
+  console.log('Connected to database.');
+});
+
+connection.end();
