34f6ddec30e59dd14e6ce75c4dc3d3577a4f9f7b
Lpereira00 <luis.pereira001589@gmail.com>
2022-03-28 14:08:19 -0600

    changes

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const test_message = process.env.MESSAGE || "testing endpoints again"
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node! My Test Pipeline Test with env file \n'
+  const msg = 'Hello Node! My Test Pipeline Test  \n'
   res.end("<p>"+msg+"</p>"+"<p>"+test_message+"</p>");
 });
 //+"<p>"+JSON.stringify(data)+"</p>"
